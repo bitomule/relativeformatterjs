83b14934000d93b16851afacea451b941dc35b73
David Collado Sela <bitomule@gmail.com>
2015-07-14 15:02:52 +0200

    interval helper

---
+++ package.js
@@ -0,0 +1,36 @@
+Package.describe({
+  name: "bitomule:relativeformatterjs",
+  summary: "",
+  version: "0.1.0",
+  git: "https://github.com/bitomule/relativeformatterjs"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom("1.0.1");
+  api.use(["grigio:babel","momentjs:moment","tap:i18n"]);
+  
+  api.addFiles("lib/relativeformatter.es6.js", ["client","server"]);
+  api.add_files("package-tap.i18n", ["client", "server"]);
+  api.add_files([
+    "i18n/en.i18n.json",
+    "i18n/es.i18n.json"
+  ], ["client", "server"]);
+  api.export('RelativeFormatter', ["client","server"]);
+});
+
+    
+Package.onTest(function (api) {
+    api.use("tinytest");
+    api.use(["meteor","grigio:babel","momentjs:moment","tap:i18n"]);
+  
+    api.addFiles("lib/relativeformatter.es6.js", ["client","server"]);
+    api.add_files("package-tap.i18n", ["client", "server"]);
+    api.add_files([
+      "i18n/en.i18n.json",
+      "i18n/es.i18n.json"
+    ], ["client", "server"]);
+    
+    api.addFiles("tests/tests.js", ["client","server"]);
+});    
+  
+  
